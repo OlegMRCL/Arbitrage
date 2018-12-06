67b7d9bc8694d182f257a3c2403819b0a060d73e
OlegMRCL <helgicannoli@gmail.com>
2018-12-06 22:36:59 +0300

    Rewrite getPairs function to get numeric data as float64

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"net/http"
 	"io/ioutil"
 	"encoding/json"
+	"fmt"
+	"strconv"
 )
 
 type App struct {
@@ -29,27 +31,36 @@ func (app *App) getPairs() (map[string]Pair, error) {
 	resp, _ := app.Get("https://api.exmo.com/v1/ticker/")
 	defer resp.Body.Close()
 	body, _ := ioutil.ReadAll(resp.Body)
-	result := make(map[string]Pair)
+	result := make(map[string]struct{Bid string `json:"Buy_price"`; Ask string `json:"Sell_price"`})
 	err := json.Unmarshal(body, &result)
-	return result, err
+
+	pairs := make (map[string]Pair)
+	for key, value := range result {
+		p := new(Pair)
+		p.Bid, _ = strconv.ParseFloat(value.Bid, 64)
+		p.Ask, _ = strconv.ParseFloat(value.Ask, 64)
+		pairs[key] = *p
+	}
+
+	return pairs, err
 }
 
 
 
 type Pair struct {
-	Buy_price  string
-	Sell_price string
-	//Last_trade string
-	//High string
-	//Low string
-	//Avg string
-	//Vol string
-	//Vol_curr string
+	Bid  float64
+	Ask float64
+	//Last_trade float64
+	//High float64
+	//Low float64
+	//Avg float64
+	//Vol float64
+	//Vol_curr float64
 	//Updated int
 }
 
 
-
 func main() {
-
+	app := new(App)
+	fmt.Println(app.getPairs())
 }
