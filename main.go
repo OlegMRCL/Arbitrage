128047b1706d5d3171d4439b9718d48e549c6922
OlegMRCL <helgicannoli@gmail.com>
2018-12-11 00:55:23 +0300

    Add showChains function

--- main.go
+++ main.go
@@ -17,7 +17,6 @@ type App struct {
 }
 
 
-//This function
 func (app *App) getCurrencies() ([]string, error) {
 	resp, _ := app.Get("https://api.exmo.com/v1/currency/")
 	defer resp.Body.Close()
@@ -55,13 +54,6 @@ func (app *App) getPairs() (pairList, error) {
 type Pair struct {
 	Bid  float64
 	Ask float64
-	//Last_trade float64
-	//High float64
-	//Low float64
-	//Avg float64
-	//Vol float64
-	//Vol_curr float64
-	//Updated int
 }
 
 
@@ -69,9 +61,9 @@ type pairList map[string]Pair
 
 
 type chain struct {
-	multiply float64
+	product  float64
 	kProfit  float64
-	nextLink int8
+	nextLink uint8
 }
 
 
@@ -91,9 +83,9 @@ func (pl *pairList)  generateTable (currencies []string) Table {
 }
 
 
-func getInd (c []string, currencies []string) (int8, int8) {
-	var i, j int8
-	k := int8(0)
+func getInd (c []string, currencies []string) (uint8, uint8) {
+	var i, j uint8
+	k := uint8(0)
 	for foundInd := 0; foundInd < 2; {
 		switch {
 		case currencies[k] == c[0]:
@@ -113,46 +105,66 @@ type Table [][]chain
 
 
 func (t *Table) FloydWarshall() {
-	nCurrency := len(*t)
-	for k := 0; k < nCurrency; k++ {
-		for i := 0; i < nCurrency; i++ {
-			for j := 0; j < nCurrency; j++ {
-				IJ := &(*t)[i][j]
-				if (IJ.kProfit != 0) && (i != j) && (i != k) && (j != k) {
-					IK := &(*t)[i][k]
-					KJ := &(*t)[k][j]
-					if (IK.multiply * KJ.multiply) > IJ.multiply {
-						IJ.multiply = IK.multiply * KJ.multiply * (1 - fee)
-						IJ.nextLink = IK.nextLink
-					}
-				}
-
+	nCurrency := uint8(len(*t))
+	for k := uint8(0); k < nCurrency; k++ {
+		for i := uint8(0); i < nCurrency; i++ {
+			for j := uint8(0); j < nCurrency; j++ {
+				(*t)[i][j] = t.compareChains(i, j, k)
 			}
 		}
 	}
 }
 
-func main() {
-	app := new(App)
-	currencies, _ := app.getCurrencies()
-	pairs, _ := app.getPairs()
-	table := pairs.generateTable(currencies)
-	table.FloydWarshall()
+func (t *Table) compareChains (i, j, k uint8) chain{
+	IJ := &(*t)[i][j]
 
-	nCurrencies := int8(len(currencies))
+	if (i != j) && (i != k) && (j != k) {
+		IK := &(*t)[i][k]
+		KJ := &(*t)[k][j]
+		if (IK.product * KJ.product) > IJ.product {
+			IJ.product = IK.product * KJ.product * (1 - fee)
+			IJ.nextLink = IK.nextLink
+		}
+	}
+	return *IJ
+}
 
-	for i := int8(0); i < nCurrencies; i++ {
-		for j := int8(0); j < nCurrencies; j++ {
-			if (i != j) && (table[i][j].multiply * table[i][j].kProfit > 1) {
-				fmt.Println(table[i][j].multiply * table[i][j].kProfit)
+
+func showChains (t *Table, currencies []string) {
+	nCurrencies := uint8(len(currencies))
+	var count uint8 = 0
+	for i := uint8(0); i < nCurrencies; i++ {
+		for j := uint8(0); j < nCurrencies; j++ {
+			if (i != j) && ((*t)[i][j].product * (*t)[i][j].kProfit > 1) {
+				fmt.Println((*t)[i][j].product * (*t)[i][j].kProfit)
 				fmt.Print(currencies[i], "-->")
-				for k := table[i][j].nextLink; k != j; {
+				for k := (*t)[i][j].nextLink; k != j; {
 					fmt.Print(currencies[k], "-->")
-					k = table[k][j].nextLink
+					k = (*t)[k][j].nextLink
 				}
-				fmt.Print(currencies[j])
+				fmt.Print(currencies[j], "-->", currencies[i], "\n")
+				count++
 			}
 		}
 	}
+	if count == 0 {
+		fmt.Println("Arbitrage is not found")
+	}
+}
 
+
+
+func main() {
+	app := new(App)
+
+	//Get data from EXMO
+	currencies, _ := app.getCurrencies()
+	pairs, _ := app.getPairs()
+
+	//Find arbitrage using the data obtained
+	table := pairs.generateTable(currencies)
+	table.FloydWarshall()
+
+	//Show all found chains with profit or message about their absence
+	showChains(&table, currencies)
 }
