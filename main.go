b3a002eeacaa9d4514f25df49fd36b1f648f3ce5
OlegMRCL <helgicannoli@gmail.com>
2018-12-10 22:58:08 +0300

    Add generateTable function; Add getInd function; Finish FloydWarshall function

--- main.go
+++ main.go
@@ -6,16 +6,19 @@ import (
 	"encoding/json"
 	"fmt"
 	"strconv"
+	"strings"
 )
+
+const fee = 0.002
+
 
 type App struct {
 	http.Client
 }
 
 
-
+//This function
 func (app *App) getCurrencies() ([]string, error) {
-
 	resp, _ := app.Get("https://api.exmo.com/v1/currency/")
 	defer resp.Body.Close()
 	body, _ := ioutil.ReadAll(resp.Body)
@@ -32,7 +35,7 @@ func (app *App) getPairs() (pairList, error) {
 	defer resp.Body.Close()
 	body, _ := ioutil.ReadAll(resp.Body)
 	result := make(map[string]struct{
-		Bid string `json:"Buy_price"`;
+		Bid string `json:"Buy_price"`
 		Ask string `json:"Sell_price"`
 		})
 	err := json.Unmarshal(body, &result)
@@ -65,44 +68,91 @@ type Pair struct {
 type pairList map[string]Pair
 
 
-func (pl *pairList) findPair(currency1 string, currency2 string) Pair {
-	if _,ok := (*pl)[currency1+"_"+currency2]; ok {
-		return (*pl)[currency1+"_"+currency2]
-	}else if _,ok := (*pl)[currency2+"_"+currency1]; ok{
-		return (*pl)[currency2+"_"+currency1]
-	}else{
-		return Pair{0, 0}
-	}
-}
-
-
-type profit struct {
+type chain struct {
 	multiply float64
-	profit float64
-	nextLink string
+	kProfit  float64
+	nextLink int8
 }
 
 
+func (pl *pairList)  generateTable (currencies []string) Table {
+	nCurrency := len(currencies)
+	t := make(Table, nCurrency)
+	for k := 0; k < nCurrency; k++ {
+		t[k] = make([]chain, nCurrency)
+	}
+	for k, v := range *pl {
+		c := strings.Split(k, "_")
+		i, j := getInd(c, currencies)
+		t[i][j] = chain{v.Bid * (1 - fee), 1/v.Ask * (1 - fee), j}
+		t[j][i] = chain{1/v.Ask * (1 - fee), v.Bid * (1 - fee), i}
+	}
+	return t
+}
 
-func FloydWarshall(currencies []string) {
-	for _, k := range currencies {
-		for _, i := range currencies {
-			for _, j := range currencies {
-				fmt.Println(k, i, j)
+
+func getInd (c []string, currencies []string) (int8, int8) {
+	var i, j int8
+	k := int8(0)
+	for foundInd := 0; foundInd < 2; {
+		switch {
+		case currencies[k] == c[0]:
+			i = k
+			foundInd++
+		case currencies[k] == c[1]:
+			j = k
+			foundInd++
+		}
+		k++
+	}
+	return i, j
+}
+
+
+type Table [][]chain
+
+
+func (t *Table) FloydWarshall() {
+	nCurrency := len(*t)
+	for k := 0; k < nCurrency; k++ {
+		for i := 0; i < nCurrency; i++ {
+			for j := 0; j < nCurrency; j++ {
+				IJ := &(*t)[i][j]
+				if (IJ.kProfit != 0) && (i != j) && (i != k) && (j != k) {
+					IK := &(*t)[i][k]
+					KJ := &(*t)[k][j]
+					if (IK.multiply * KJ.multiply) > IJ.multiply {
+						IJ.multiply = IK.multiply * KJ.multiply * (1 - fee)
+						IJ.nextLink = IK.nextLink
+					}
+				}
 
 			}
 		}
 	}
 }
 
-
 func main() {
 	app := new(App)
+	currencies, _ := app.getCurrencies()
+	pairs, _ := app.getPairs()
+	table := pairs.generateTable(currencies)
+	table.FloydWarshall()
 
-	currencies, err := app.getCurrencies()
+	nCurrencies := int8(len(currencies))
 
-	pairs, err := app.getPairs()
-	fmt.Println(currencies, err)
-	fmt.Println(pairs.findPair("XMR", "GUSD"))
+	for i := int8(0); i < nCurrencies; i++ {
+		for j := int8(0); j < nCurrencies; j++ {
+			if (i != j) && (table[i][j].multiply * table[i][j].kProfit > 1) {
+				fmt.Println(table[i][j].multiply * table[i][j].kProfit)
+				fmt.Print(currencies[i], "-->")
+				for k := table[i][j].nextLink; k != j; {
+					fmt.Print(currencies[k], "-->")
+					k = table[k][j].nextLink
+				}
+				fmt.Print(currencies[j])
+			}
+		}
+	}
 
 }
